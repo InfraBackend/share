947d358b393676569df86b71bb68cfd3b6fbca55
huzhonglan <huzhonglan@HuzhonglandeMacBook-Pro.local>
2022-09-15 08:55:43 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'M',
+    description: 'Just playing around',
+    themeConfig: {
+        nav: [{
+            text: '操作系统',
+            link: '/os/' 
+        }
+    ],
+    sidebar: {
+        
+    }
+    }
+  }
